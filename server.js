1eaff856addd52459b48218d4effa17de40e7ec2
shivamIndian <sshivam212@gmail.com>
2023-05-14 14:55:58 +0530

    server commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
-import {fileUrlToPath} from 'url';
+import { fileURLToPath } from 'url';
 //configure env
 dotenv.config();
 
@@ -16,8 +16,8 @@ dotenv.config();
 connectDB();
 
 //es_modulefix
-const __filename = fileURLToPath(import.meta.url); 
-const __dirname = path.dirname(__filename); 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
